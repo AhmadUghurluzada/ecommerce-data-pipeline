d7b65047638b82e96bc60b5489bd17902879f9a2
ehmed <ahmaduguhrluzada@gmail.com>
2026-02-19 12:31:09 +0400

    Update build_dim_customers.py

---
+++ etl/build_dim_customers.py
@@ -0,0 +1,80 @@
+import pandas as pd
+
+def extract_customers():
+    """
+    Extracts customers and orders data from CSV files.
+    Returns:
+        customers (pd.DataFrame): Raw customers dataset 
+        orders (pd.DataFrame): Raw orders dataset
+    """
+    customers = pd.read_csv("data/raw/olist_customers_dataset.csv")
+    orders = pd.read_csv("data/raw/olist_orders_dataset.csv")
+    return customers, orders
+
+
+def transform_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    """
+    Transform raw data into dim_customers dimension table.
+    
+    Logic:
+    1. Merge customers with orders to get the order purchase timestamp.
+    2. Sort by most recent purchase per real customer.
+    3. Deduplicate based on customer_unique_id.
+    4. Generate surrogate key.
+    5. Select final columns.
+
+    Returns:
+        dim_customers (pd.DataFrame): Transformed customers dimension table.
+    """
+    # Merge to attach purchase timestamp
+    df = customers.merge(
+        orders[["order_id", "customer_id", "order_purchase_timestamp"]],
+        on="customer_id",
+        how="left"
+    )
+
+    # Convert to datetime
+    df["order_purchase_timestamp"] = pd.to_datetime(df["order_purchase_timestamp"])
+
+    # Sort by latest purchase per real customer
+    df = df.sort_values(
+        ["customer_unique_id", "order_purchase_timestamp"],
+        ascending=[True, False]
+    )
+
+    # Keep most recent address per real customer
+    df = df.drop_duplicates(subset="customer_unique_id", keep="first")
+
+    # Select dimension columns
+    df = df[
+        [
+            "customer_unique_id",
+            "customer_city",
+            "customer_state",
+            "customer_zip_code_prefix"
+        ]
+    ]
+
+    # Reset index and create surrogate key
+    df = df.reset_index(drop=True)
+    df["customer_id"] = df.index + 1
+
+    return df
+
+
+def load(df: pd.DataFrame):
+    """
+    Placeholder load function.
+    For now, we just display summary.
+    Later, this will insert into PostgreSQL.
+    """
+    print("Final dim_customers row count:", len(df))
+    print(df.head())
+    print("Unique customer_unique_id:", df["customer_unique_id"].nunique())
+
+
+
+if __name__ == "__main__":
+    customers_raw, orders_row = extract_customers()
+    dim_customers = transform_customers(customers_raw, orders_row)
+    load(dim_customers)
